c012a6568464d862a9a8cc18353fcbd159f62233
Nik Silver <nik@niksilver.com>
2012-11-14 22:45:44 +0000

    Now properly removed unnecessary parens, and added new test suite.

---
+++ src/test/scala/org/pigsaw/eigendmapper/BCatMapperSuite.scala
@@ -0,0 +1,28 @@
+package org.pigsaw.eigendmapper
+
+import org.scalatest.FunSuite
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import org.pigsaw.eigendmapper._
+import org.pigsaw.eigendmapper.Mapper._
+
+@RunWith(classOf[JUnitRunner])
+class BCatMapperSuite extends FunSuite {
+
+  trait TestParser extends BCatParser {
+    def parseOption[T](parser: Parser[T], dictstr: String): Option[T] =
+      parseAll(parser, dictstr) match {
+        case Success(out, _) => Some(out)
+        case fail => None
+      }
+  }
+
+  test("Read bcat line - text") {
+    new TestParser {
+      // Insert tests here
+    }
+  }
+
+}
