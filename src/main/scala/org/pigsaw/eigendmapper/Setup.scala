6a15fc75300e4a61e3e45897571db962cdb4f312
Nik Silver <nik@niksilver.com>
2012-12-04 11:58:09 +0000

    Broke out Setup into its own file.

---
+++ src/main/scala/org/pigsaw/eigendmapper/Setup.scala
@@ -0,0 +1,75 @@
+package org.pigsaw.eigendmapper
+
+/**
+ * A particular set of connections
+ */
+class Setup(val conns: Set[Connection]) {
+
+  /**
+   * Get all the agent names mentioned in the set of connections,
+   * including the angle brackets.
+   */
+  lazy val agents: Set[String] =
+    conns flatMap { _.agents }
+      
+  /**
+   * Get all the ports named in the connections.
+   */
+  lazy val ports: Set[Port] =
+    conns flatMap { c => List(c.master, c.slave) }
+  
+  /**
+   * Get a map from each agent to each agent (strings, including
+   * angle brackets.)
+   */
+  lazy val agentAgentConnections: Set[(String, String)] =
+    conns map { c => (c.master.agent getOrElse "UNKNOWN", c.slave.agent getOrElse "UNKNOWN") }
+
+  /**
+   * Get a map from each agent (a string including angle brackets)
+   * to all its ports.
+   */
+  lazy val agentPortConnections: Set[(String, Port)] =
+    ports map { p => ((p.agent getOrElse "UNKNOWN") -> p) }
+  
+  /**
+   * Create a unified set of connections. This means if any connections
+   * carry a port name, then those names are applied wherever those
+   * ports are used.
+   */
+  def unified: Setup = {
+    val ports = conns flatMap { c => List(c.master, c.slave) }
+    val namingPorts = ports filter (_.name.nonEmpty)
+    val names: Map[String, String] = namingPorts map { p => (p.id -> p.name.get) } toMap
+
+    // Produce an updated version of the port, with names filled in if available.
+    def updated(port: Port): Port = {
+      if (port.name.nonEmpty) port
+      else Port(port.id, names.get(port.id))
+    }
+
+    val updatedConns = conns map (c => Connection(updated(c.master), updated(c.slave)))
+    new Setup(updatedConns)
+  }
+
+  /**
+   * Make a normalised version of this set of connections, in which
+   * every port of the form ID &lt;main:agentnameN&gt; is changed to
+   * its shorter form of &lt;agentnameN&gt;.
+   */
+  def normalised: Setup =
+    new Setup(conns map { _.normalised })
+
+  /**
+   * Get the rigs (other setups) inside this one.
+   * Each one is mapped from its name, such as &lt;rig2&gt;.
+   */
+  def rigs: Map[String, Setup] = Map()
+}
+
+object Setup {
+  /**
+   * Produce a normalised setup.
+   */
+  def apply(conns: Set[Connection]): Setup = new Setup(conns).normalised.unified
+}
