89f93a93fc9a0b99e7a6c74c0c4a08cf67cbd01e
Nik Silver <nik@niksilver.com>
2012-12-14 21:25:14 +0000

    Restored preamble, with Pos and AgentName classes.

--- src/main/scala/org/pigsaw/eigendmapper/Preamble.scala
+++ src/main/scala/org/pigsaw/eigendmapper/Preamble.scala
@@ -2,32 +2,31 @@ package org.pigsaw.eigendmapper
 
 object Preamble {
   /**
-   * An agent name, including the angle brackets.
+   * The name of an agent, including the angle brackets.
    */
   case class AgentName(name: String) {
-    /** Remove the start and end angle brackets, if any. */
+    /**
+     * Get the name without the angle brackets (if any).
+     */
     def withoutBrackets: String = {
       val strip1 = name.dropWhile(_ == '<')
-      if (strip1.last == '>') strip1.init else strip1
+      if (strip1.endsWith(">")) strip1.init else strip1
     }
-    // ("<(.*)>".r findFirstMatchIn name).get.group(1)
   }
 
-  implicit def String2AgentName(name: String): AgentName =
-    AgentName(name)
+  implicit def String2AgentName(s: String) = new AgentName(s)
 
   /**
-   * The position in the rig hierarchy of setups.
-   * An empty list means the top level; List("&lt;rig3&gt")
+   * A position in a rig hierarchy in which we're currently
+   * interested. An empty list means the top level; List("&lt;rig3&gt")
    * means rig3 within the top level, and so on.
    */
   case class Pos(p: String*) {
-
     /**
      * Convert a pos to an index specification for the bls command:
-     * Pos() =>         <main>
-     * Pos("<rig1>") => <main.rig1:main>
-     * Pos("<rig2>") => <main.rig1:main.rig2:main>
+     * List() =>         <main>
+     * List("<rig1>") => <main.rig1:main>
+     * List("<rig2>") => <main.rig1:main.rig2:main>
      */
     def index: String = {
       def insertMains(s: String) = "." + s.withoutBrackets + ":main"
@@ -35,5 +34,5 @@ object Preamble {
     }
   }
   
-  implicit def ListString2Pos(p: List[String]): Pos = Pos(p: _*)
+  implicit def ListString2Pos(p: List[String]) = Pos(p: _*)
 }
