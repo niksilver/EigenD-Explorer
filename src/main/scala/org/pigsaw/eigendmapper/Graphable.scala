5d9a248b28be6d30e135075c23fceb6945ad1902
Nik Silver <nik@niksilver.com>
2013-01-05 19:51:54 +0000

    A bit of tidying.

--- src/main/scala/org/pigsaw/eigendmapper/Graphable.scala
+++ src/main/scala/org/pigsaw/eigendmapper/Graphable.scala
@@ -44,29 +44,28 @@ object Graphable {
   class GPortPort(c: Connection) {
     lazy val xmlId: String = c.master.portXmlId + c.slave.portXmlId
 
-    lazy val edgeXML: String = {
-      edgeXMLTemplate.format(GPortPort(c).xmlId, c.master.portXmlId, c.slave.portXmlId, portPortEdgeWeight)
-    }
+    lazy val edgeXML: String =
+      edgeXMLTemplate.format(xmlId, c.master.portXmlId, c.slave.portXmlId, portPortEdgeWeight)
   }
 
   class GAgentPort(ap: (String, String)) {
     lazy val xmlId: String = ap._1.stringXmlId + ap._2.portXmlId
     
     lazy val edgeXML: String =      
-      edgeXMLTemplate.format(GAgentPort(ap).xmlId, ap._1.stringXmlId, ap._2.portXmlId, portAgentEdgeWeight)
+      edgeXMLTemplate.format(xmlId, ap._1.stringXmlId, ap._2.portXmlId, portAgentEdgeWeight)
   }
 
   class GPortAgent(pa: (String, String)) {
     lazy val xmlId: String = pa._1.portXmlId + pa._2.stringXmlId
     
     lazy val edgeXML: String =
-      edgeXMLTemplate.format(GPortAgent(pa).xmlId, pa._1.portXmlId, pa._2.stringXmlId, portAgentEdgeWeight)
+      edgeXMLTemplate.format(xmlId, pa._1.portXmlId, pa._2.stringXmlId, portAgentEdgeWeight)
   }
 
   class GAgentAgent(aa: (String, String)) {
     lazy val xmlId: String = aa._1.stringXmlId + aa._2.stringXmlId
 
     lazy val edgeXML: String =
-      edgeXMLTemplate.format(GAgentAgent(aa).xmlId, aa._1.stringXmlId, aa._2.stringXmlId, agentAgentEdgeWeight)
+      edgeXMLTemplate.format(xmlId, aa._1.stringXmlId, aa._2.stringXmlId, agentAgentEdgeWeight)
   }  
 }
